690e1eb80ca76c7b35901b2d55d02da40ec5b046
Leif Johansson <leifj@sunet.se>
2025-06-24 18:46:08 +0200

    no longer needed

--- pkg/etsi119612/schema.go
+++ pkg/etsi119612/schema.go
@@ -4,10 +4,6 @@ type SignaturePolicyImplied AnyType
 type AllSignedDataObjects AnyType
 type Lang string
 
-func (Lang) Validate() error {
-	return nil
-}
-
 func FindByLanguage(names *InternationalNamesType, lang string, dflt string) string {
 	for _, n := range names.Name {
 		if string(*n.XmlLangAttr) == lang {
