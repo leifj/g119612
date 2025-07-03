ab356f8c3b49e7ceaa0d71d3f39f94983e2d3c06
Anna Alexeeva <anna@sunet.se>
2025-07-03 16:43:12 +0200

    Added a test case with signed TL

--- pkg/etsi119612/x5c_validation_test.go
+++ pkg/etsi119612/x5c_validation_test.go
@@ -38,14 +38,13 @@ func TestLeafRootCertVerificationSuccess(t *testing.T) {
 	gock.New("https://ewc-consortium.github.io").
 		Get("/EWC-TL").
 		Reply(200).
-		File("testdata/test-trust-list.xml")
+		File("testdata/test-trust-list-no-sig.xml")
 	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
+	assert.NoError(t,err)
 	policy := *etsi119612.PolicyAll 
 	policy.AddServiceTypeIdentifier("http://uri.etsi.org/TrstSvc/Svctype/CA/QC")
 	pool := tsl.ToCertPool(&policy)
-
 	fmt.Println("Number of trusted roots:", len(pool.Subjects()))
-	fmt.Println(pool)
 	assert.NotNil(t, pool)
 	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
 	assert.NoError(t,err)
@@ -81,15 +80,13 @@ func TestLeafIntermediateRootCertVerificationSuccess(t *testing.T) {
 	gock.New("https://ewc-consortium.github.io").
 		Get("/EWC-TL").
 		Reply(200).
-		File("testdata/test-trust-list.xml")
+		File("testdata/test-trust-list-no-sig.xml")
 	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
+    assert.NoError(t,err)
 	policy := *etsi119612.PolicyAll 
 	policy.AddServiceTypeIdentifier("http://uri.etsi.org/TrstSvc/Svctype/CA/QC")
-	fmt.Println("ServiceTypeIdentifier:", policy.ServiceTypeIdentifier)
 	pool := tsl.ToCertPool(&policy)
-
 	fmt.Println("Number of trusted roots:", len(pool.Subjects()))
-	fmt.Println(pool)
 	assert.NotNil(t, pool)
 	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
 	assert.NoError(t,err)
@@ -112,11 +109,7 @@ func TestLeafIntermediateRootCertVerificationSuccess(t *testing.T) {
 
 func TestLeafRootCertVerificationSuccessEmptyServiceTypeIdentifier(t *testing.T) {
 	header_mock, err:= os.ReadFile("./testdata/x5c-test-root-leaf.json")
-	if err != nil {
-		t.Fatalf("Failed while reading json: %v", err)
-	}
 	assert.NoError(t, err)
-
     assert.NotEmpty(t, header_mock) 
 	var jwt JWTCertBundle
 	err =json.Unmarshal(header_mock, &jwt)
@@ -129,12 +122,46 @@ func TestLeafRootCertVerificationSuccessEmptyServiceTypeIdentifier(t *testing.T)
 	gock.New("https://ewc-consortium.github.io").
 		Get("/EWC-TL").
 		Reply(200).
-		File("testdata/test-trust-list.xml")
+		File("testdata/test-trust-list-no-sig.xml")
 	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
-	
+	assert.NoError(t,err)
 	pool := tsl.ToCertPool(etsi119612.PolicyAll)
 	fmt.Println("Number of trusted roots:", len(pool.Subjects()))
-	fmt.Println(pool)
+	assert.NotNil(t, pool)
+	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
+	assert.NoError(t,err)
+	leafCert, err := x509.ParseCertificate(leafDER)
+	assert.NoError(t,err)
+	_, err = leafCert.Verify(x509.VerifyOptions{
+    Roots: pool})
+	if err !=nil {
+		t.Errorf("Chain verification failed %v", err)
+	} else {
+		fmt.Println("Chain verification succeeded")
+	}
+}
+
+func TestLeafRootCertVerificationSuccessTLWithSignature(t *testing.T) {
+	header_mock, err:= os.ReadFile("./testdata/x5c-test-root-leaf.json")
+	assert.NoError(t, err)
+    
+	assert.NotEmpty(t, header_mock) 
+	var jwt JWTCertBundle
+	err =json.Unmarshal(header_mock, &jwt)
+	assert.NoError(t, err, "Failed to unmarshal JWT bundle")
+	assert.NotEmpty(t, jwt)
+	assert.NotEmpty(t, jwt.Alg) 
+	assert.NotEmpty(t, jwt.Typ)
+	assert.NotEmpty(t, jwt.X5c)
+	defer gock.Off()
+	gock.New("https://ewc-consortium.github.io").
+		Get("/EWC-TL").
+		Reply(200).
+		File("testdata/test-trust-list-with-sig.xml")
+	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
+	assert.NoError(t,err)
+	pool := tsl.ToCertPool(etsi119612.PolicyAll)
+	fmt.Println("Number of trusted roots:", len(pool.Subjects()))
 	assert.NotNil(t, pool)
 	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
 	assert.NoError(t,err)
