f055173161bf1433257bec4bf2da9abe22fe0c30
Anna Alexeeva <anna@sunet.se>
2025-07-04 16:02:33 +0200

    Test cases TSL methods and Error on Validate

--- pkg/etsi119612/x5c_validation_test.go
+++ pkg/etsi119612/x5c_validation_test.go
@@ -175,3 +175,73 @@ func TestLeafRootCertVerificationSuccessTLWithSignature(t *testing.T) {
 		fmt.Println("Chain verification succeeded")
 	}
 }
+
+func TestServiceStatusOtherThanGrantedStatusError(t *testing.T){
+	header_mock, err:= os.ReadFile("./testdata/x5c-test-root-leaf.json")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, header_mock)
+	var jwt JWTCertBundle
+	err =json.Unmarshal(header_mock, &jwt)
+	assert.NoError(t, err, "Failed to unmarshal JWT bundle")
+	assert.NotEmpty(t, jwt)
+	assert.NotEmpty(t, jwt.Alg)
+	assert.NotEmpty(t, jwt.Typ)
+	assert.NotEmpty(t, jwt.X5c)
+	defer gock.Off()
+	gock.New("https://ewc-consortium.github.io").
+		Get("/EWC-TL").
+		Reply(200).
+		File("testdata/test-trust-list-with-sig.xml")
+	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
+	policy := *etsi119612.PolicyAll
+	policy.AddServiceStatus("https://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/other-than-granted/")
+	fmt.Println(policy.ServiceStatus)
+	//keep only other-than-granted in the slice
+	if len(policy.ServiceStatus) > 0 {
+    policy.ServiceStatus = policy.ServiceStatus[1:]}
+	fmt.Println("Status to test:", policy.ServiceStatus)
+	pool:= tsl.ToCertPool(&policy)
+	assert.NotNil(t, pool)
+	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
+	assert.NoError(t,err)
+	leafCert, err := x509.ParseCertificate(leafDER)
+	assert.NoError(t,err)
+	_, err = leafCert.Verify(x509.VerifyOptions{
+    Roots: pool})
+	assert.Error(t,err, "status is not recognized or granted")
+}
+func TestServiceStatusOneOfInTheListSuccess(t *testing.T){
+	header_mock, err:= os.ReadFile("./testdata/x5c-test-root-leaf.json")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, header_mock)
+	var jwt JWTCertBundle
+	err =json.Unmarshal(header_mock, &jwt)
+	assert.NoError(t, err, "Failed to unmarshal JWT bundle")
+	assert.NotEmpty(t, jwt)
+	assert.NotEmpty(t, jwt.Alg)
+	assert.NotEmpty(t, jwt.Typ)
+	assert.NotEmpty(t, jwt.X5c)
+	defer gock.Off()
+	gock.New("https://ewc-consortium.github.io").
+		Get("/EWC-TL").
+		Reply(200).
+		File("testdata/test-trust-list-with-sig.xml")
+	tsl, err := etsi119612.FetchTSL("https://ewc-consortium.github.io/ewc-trust-list/EWC-TL")
+	assert.NoError(t, err)
+	policy := *etsi119612.PolicyAll
+	policy.AddServiceStatus("https://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/other-than-granted/")
+	fmt.Println(policy.ServiceStatus)
+	pool:= tsl.ToCertPool(&policy)
+	assert.NotNil(t, pool)
+	leafDER, err:= base64.StdEncoding.DecodeString(jwt.X5c[0])
+	assert.NoError(t,err)
+	leafCert, err := x509.ParseCertificate(leafDER)
+	assert.NoError(t,err)
+	_, err = leafCert.Verify(x509.VerifyOptions{
+    Roots: pool})
+	if err !=nil {
+		t.Errorf("Chain verification failed %v", err)
+	} else {
+		fmt.Println("Chain verification succeeded")
+	}
+}
